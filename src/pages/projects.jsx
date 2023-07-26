aa26899e5ef74bc13305d9a9a344ee1ebe9b4921
garciazapiain <juangarciazapiain@gmail.com>
2023-07-26 13:51:03 +0200

    added nectario

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,17 +1,12 @@
-import Head from 'next/head'
 import Image from 'next/image'
 
 import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
-import logoAnimaginary from '@/images/logos/animaginary.svg'
-import logoCosmos from '@/images/logos/cosmos.svg'
-import logoHelioStream from '@/images/logos/helio-stream.svg'
-import logoOpenShuttle from '@/images/logos/open-shuttle.svg'
-import logoPlanetaria from '@/images/logos/planetaria.svg'
 import { Button } from '@/components/Button'
 import recurringGif from '../gifs/recurring.gif'
 import czechCasesGif from '../gifs/czech-cases.gif'
 import panoramapGif from '../gifs/panoramap.gif'
+import nectarioGif from '../gifs/nectario.gif'
 import todoGif from '../gifs/todo.gif'
 
 const projects = [
@@ -20,17 +15,24 @@ const projects = [
         description:
             'Simplifies the process of managing your recurring purchases.',
         link: { href: 'http://planetaria.tech', label: 'planetaria.tech' },
-        logo: logoPlanetaria,
         repoLink: "https://github.com/garciazapiain/recurring-manager-fullstack-app",
-        demoLink: "https://recurring-manager-app.herokuapp.com/",
+        websiteLink: "https://recurring-manager-app.herokuapp.com/",
         gif: recurringGif
+    },
+    {
+        name: 'Nectario',
+        description:
+            'Website for a local family owned restaurant.',
+        link: { href: '#', label: 'github.com' },
+        repoLink: "https://github.com/garciazapiain/nectario",
+        websiteLink: "https://www.nectario.mx/",
+        gif: nectarioGif
     },
     {
         name: 'Czech Cases Practice',
         description:
             'Helps Czech language learners practice noun declension.',
         link: { href: '#', label: 'github.com' },
-        logo: logoAnimaginary,
         repoLink: "https://github.com/garciazapiain/czech-cases-practice",
         demoLink: "https://garciazapiain.github.io/czech-cases-practice/",
         gif: czechCasesGif,
@@ -40,7 +42,6 @@ const projects = [
         description:
             'Take a glance at a panoramic view and get more info of what you see.',
         link: { href: '#', label: 'github.com' },
-        logo: logoHelioStream,
         repoLink: "https://github.com/garciazapiain/panoramap",
         demoLink: "https://panoramap-pn0bg9ng9-garciazapiain.vercel.app/",
         gif: panoramapGif
@@ -50,7 +51,6 @@ const projects = [
         description:
             'The classic :)',
         link: { href: '#', label: 'github.com' },
-        logo: logoCosmos,
         repoLink: "https://github.com/garciazapiain/todo-app",
         demoLink: "https://garciazapiain.github.io/todo-app/",
         gif: todoGif
@@ -78,10 +78,18 @@ export default function Projects() {
                                 <Image src={project.gif} alt="My Gif" />
                             </div>
                             <Card.Description>{project.description}</Card.Description>
-                            <div className='flex mt-2 w-full'>
-                                {project.demoLink && <Button href={project.demoLink} target="a_blank" className='mr-2 w-1/2'>Demo</Button>}
-                                <Button href={project.repoLink} target="a_blank" className='w-1/2'>Repo</Button>
-                            </div>
+                            {project.demoLink &&
+                                <div className='flex h-full items-end mt-2 w-full'>
+                                    <Button href={project.demoLink} target="a_blank" className='mr-2 w-1/2'>Demo</Button>
+                                    <Button href={project.repoLink} target="a_blank" className='w-1/2'>Repo</Button>
+                                </div>
+                            }
+                            {project.websiteLink &&
+                                <div className='flex h-full items-end mt-2 w-full'>
+                                    {<Button href={project.websiteLink} target="a_blank" className='mr-2 w-1/2'>Website</Button>}
+                                    <Button href={project.repoLink} target="a_blank" className='w-1/2'>Repo</Button>
+                                </div>
+                            }
                         </Card>
                     ))}
                 </ul>
